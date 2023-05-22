a0ce26b82352457f667e26b307c8b2071a9fd72e
Serco Honan <ashyir@yahoo.com>
2023-05-22 18:02:31 +0700

    Change logo and colors.

--- js/color.js
+++ js/color.js
@@ -1,6 +1,14 @@
+/*
 const colors = [
     "#1B4B93", // Blue.
     "#040404", // Black.
     "#FBDB04", // Yellow.
     "#E4242B", // Red.
+];
+*/
+
+const colors = [
+    "#E5173F", // Red.
+    "#133880", // Dark Blue.
+    "#147DBB", // Blue.
 ];
